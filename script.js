d8cd2d55e50365bb57e473951e6be0f8669155f6
subash pariyar <pariyarsubash2055@gmail.com>
2023-12-17 20:04:03 -0500

    modifying resposive design

--- script.js
+++ script.js
@@ -60,8 +60,6 @@ const checkWinner = () => {
     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
-      } else {
-        message.innerHTML = "game draw";
       }
     }
   }
